74370716a62e5a3d07c9c50c5aca723946287fcb
Teemu Kostamo <M6242@student.jamk.fi>
2019-10-12 11:53:11 +0300

    edited config

--- backend/config/config.js
+++ backend/config/config.js
@@ -7,11 +7,17 @@ let DB_URI = process.env.DB_URI;
 let DB_URI_GOOGLE = process.env.DB_URI_GOOGLE;
 let DB_SECRET = process.env.DB_SECRET;
 
-let CLIENT_CERT = process.env.CLIENT_CERT.replace(/\\n/g, '\n');
-let CLIENT_KEY = process.env.CLIENT_KEY.replace(/\\n/g, '\n');
-let SERVER_CA = process.env.SERVER_CA.replace(/\\n/g, '\n');
+let CLIENT_CERT = process.env.CLIENT_CERT;
+let CLIENT_KEY = process.env.CLIENT_KEY;
+let SERVER_CA = process.env.SERVER_CA;
+
+CLIENT_CERT = CLIENT_CERT.replace(/\\n/g, '\n');
+CLIENT_KEY = CLIENT_KEY.replace(/\\n/g, '\n');
+SERVER_CA = SERVER_CA.replace(/\\n/g, '\n');
 
 console.log('client cert', CLIENT_CERT);
+console.log('client key', CLIENT_KEY);
+console.log('server ca', SERVER_CA);
 
 if (process.env.NODE_ENV === 'test') {
   DB_URI = process.env.TEST_DB_URI;
