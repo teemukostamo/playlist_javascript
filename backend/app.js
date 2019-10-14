afc01a9eeb2096dd12046e98b0ff661199af4515
Teemu Kostamo <M6242@student.jamk.fi>
2019-10-14 17:35:03 +0300

    new attempt at serving index

--- backend/app.js
+++ backend/app.js
@@ -18,10 +18,22 @@ db.authenticate()
   .then(() => console.log('Database connected...'))
   .catch(err => console.log('Error: ' + err));
 
-app.use(express.static(path.resolve(__dirname, '../frontend/build')));
+console.log(path.resolve(__dirname, '../', 'frontend', 'build', 'index.html'));
+console.log(path.resolve(__dirname, '../frontend/build/index.html'));
+console.log(path.join(__dirname, '../frontend', '/build/index.html'));
+
+app.use(
+  express.static(
+    path.resolve(
+      '/Users/MacBookPro/Sites/fullstack_harjoitustyo/frontend/build/'
+    )
+  )
+);
 app.get('/', function(req, res) {
   res.sendFile(
-    path.resolve(__dirname, '../', 'frontend', 'build', 'index.html'),
+    path.resolve(
+      '/Users/MacBookPro/Sites/fullstack_harjoitustyo/frontend/build/index.html'
+    ),
     err => {
       if (err) {
         res.status(500).send(err);
