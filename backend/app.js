10262c0772e62b71127817817945ad11e547c9d8
Teemu Kostamo <M6242@student.jamk.fi>
2019-10-14 16:18:43 +0300

    reverted back to backend

--- backend/app.js
+++ backend/app.js
@@ -19,13 +19,6 @@ db.authenticate()
   .catch(err => console.log('Error: ' + err));
 
 app.use(express.static(path.resolve(__dirname, '../frontend/build')));
-// production mode
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.resolve(__dirname, '../frontend/build')));
-  app.get('*', (req, res) => {
-    res.sendfile(path.resolve((__dirname = '../frontend/build/index.html')));
-  });
-}
 
 app.use(bodyParser.json());
 app.use(cors());
