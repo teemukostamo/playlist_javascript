5f170418981fcfb8ec46a9d3c454396c567db7a3
Teemu Kostamo <teemu.kostamo@hotmail.com>
2020-10-15 19:01:06 +0300

    commented out old certs

--- backend/config/config.js
+++ backend/config/config.js
@@ -11,9 +11,9 @@ const DB_SECRET = process.env.DB_SECRET || 'salainen';
 const DB_USER = process.env.DB_USER || 'root';
 
 // SSL keys decoded from base64 for heroku
-const CLIENT_CERT = Buffer.from(process.env.CLIENT_CERT, 'base64');
-const CLIENT_KEY = Buffer.from(process.env.CLIENT_KEY, 'base64');
-const SERVER_CA = Buffer.from(process.env.SERVER_CA, 'base64');
+// const CLIENT_CERT = Buffer.from(process.env.CLIENT_CERT, 'base64');
+// const CLIENT_KEY = Buffer.from(process.env.CLIENT_KEY, 'base64');
+// const SERVER_CA = Buffer.from(process.env.SERVER_CA, 'base64');
 
 // let client_key_base64data = client_key_for_heroku.toString('base64');
 // console.log('Client key converted to base 64 is:\n\n', client_key_base64data);
@@ -38,7 +38,7 @@ module.exports = {
   DB_SECRET,
   DB_USER,
   PORT,
-  CLIENT_KEY,
-  CLIENT_CERT,
-  SERVER_CA,
+  // CLIENT_KEY,
+  // CLIENT_CERT,
+  // SERVER_CA,
 };
